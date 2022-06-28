966b65d33dea6eb8a5759768eaab75348e67783e
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2022-06-28 15:52:37 -0400

    simplified

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+//var jwt  = require("express-jwt");
+//var jwks = require("jwks-rsa");
+
+// var jwtCheck = jwt({
+//       secret: jwks.expressJwtSecret({
+//           cache: true,
+//           rateLimit: true,
+//           jwksRequestsPerMinute: 5,
+//           jwksUri: 'https://dev-ahz4kfiq.us.auth0.com/.well-known/jwks.json'
+//     }),
+//     audience: 'http://192.168.1.225/api-docs',
+//     issuer: 'https://dev-ahz4kfiq.us.auth0.com/',
+//     algorithms: ['RS256']
+// });
+
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -14,14 +29,29 @@ const swaggerUi = require("swagger-ui-express"),
    swaggerDocument = require("./swagger.json");
 
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the AlphaNumeric API." });
-});
+//app.get("/", (req, res) => {
+//  res.json({ message: "Welcome to the AlphaNumeric API." });
+//});
 
-require("./app/routes/state.routes.js")(app);
+//require("./app/routes/letter.routes.js")(app);
+//require("./app/routes/number.routes.js")(app);
+
+//app.use(jwtCheck);
 
 // swagger path to api documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.get('/authorized', function (req, res) {
+//     res.send('Secured Resource');
+// });
+
+app.get("/api/v1/letters", (req, res) => {
+    res.json({ letters: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z" });
+});
+
+app.get("/api/v1/numbers", (req, res) => {
+    res.json({ numbers: "1,2,3,4,5,6,7,8,9,0" });
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
